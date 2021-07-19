ad9869daf2534c7db431988abfd337df4675d9d9
ruby-619 <funghiju@gmail.com>
2021-07-19 15:55:28 +0800

    eddy修好勾選惹

--- src/pages/Event/CalendarChild2.js
+++ src/pages/Event/CalendarChild2.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import LunarPhaseNavbar from '../../components/LunarPhaseNavbar'
 import Footer from '../../components/Footer'
 import FilterBar from './components/FilterBar'
-import { data } from 'jquery'
+
 
 
 
@@ -17,6 +17,7 @@ const CalendarChild2 = () => {
     const [priceRange, setPriceRange] = useState('所有')
     const priceRangeTypes = ['所有', '1萬以下', '1~2萬']
     const _ = require('lodash');
+    const [state, setstate] = useState()
 
 
     async function getEventFromServer() {
@@ -32,7 +33,7 @@ const CalendarChild2 = () => {
           method: 'GET',
           headers: new Headers({
             Accept: 'application/json',
-            'Content-Type': 'appliaction/json',
+            'Content-Type': 'application/json', //這裡是application
           }),
         })
     
@@ -49,25 +50,27 @@ const CalendarChild2 = () => {
       useEffect(() => {
         getEventFromServer()
       }, [])
+      
+      useEffect(() => {
+        let newSymptom = []
+        // 處理勾選標記
+        newSymptom = handleTags(symptom, tags)
+        setdisplaySymptom(newSymptom)
+      }, [symptom,tags])
+    
 
       // 事件處理
       const handleTags = (symptom, tags) => {
         let newSymptom = [...symptom]
     
         // 處理勾選標記
+        console.log(tags)
+        console.log(symptom)
         if (tags.length > 0) {
-            newSymptom = [...newSymptom].filter((symptom) => {
-            let isFound = false
-            // 原本資料裡的tags字串轉為陣列
-            const Tags = symptom.tags.split(',')
-    
-            for (let i = 0; i < tags.length; i++) {
-              if (Tags.includes(tags[i])) {
-                return true
-              }
-            }
-    
-            return isFound
+          // console.log(123)
+            newSymptom = symptom.filter((symptom) => {
+              console.log(symptom.tags)
+            return tags.includes(symptom.tags)
           })
         }
     
@@ -92,75 +95,29 @@ const CalendarChild2 = () => {
                     />
                 </div>
                 </div>
-                {displaySymptom?.map((s, i) => {
-            return (
-             
-                <div class="ecard2 mt-5 d-flex">
-                  <div class="photo2">
-                    {/* <img src={`/img/Event/${v.eventImg}`} /> */}
-                  </div>
-                  <div class="text">
-                  
-                    {/* <h4 onClick={()=>{props.history.push(`/event-detail/${v.id}`)} }className="TextAlignLeft">{s.tags}</h4> */}
+                {displaySymptom.length && displaySymptom.map((s, i) => {
+
+              return (
                 
-                    <div class="line2 d-flex justify-content-between align-items-center border-bottom pb-3 pt-3">
-                      <div className="h6-tc d-flex align-items-center">
-                        <div>
-                          {/* <IoMdCalendar size="25px" /> */}
+                  <div class="container">
+                    <div className="row">
+                      <div className="card" style={{ width: "18rem" }}>
+                        <img className="card-img-top" src="https://fakeimg.pl/250x100/" alt="Card image cap" />
+                        <div className="card-body">
+                        <h6 className="card-title">#{s.tags}</h6>
+                        <h5 className="card-title">{s.SymptomArticleTitle}</h5>
+                        <p className="card-text">
+                        {s.SymptomArticleDescription}<br/>
+                        {s.SymptomArticleContent}
+                        </p>
+                        <a href="#" className="btn btn-primary">
+                        Read more
+                        </a>
                         </div>
-                        {/* 活動日期：
-                        {moment(v.eventDate).format('YYYY-MM-DD')}
-                        <span>({moment(v.eventDate).format('dddd')})</span> */}
                       </div>
-                    </div>
-                    <div class="line1 d-flex justify-content-between align-items-center mt-3 border-bottom">
-                      <h3>{s.symptomArticleTitle}</h3>
-                      <div className="d-flex align-items-center">
-                        {/* <div>
-                          {collection[i] === 1 ? (
-                            <BsBookmark
-                              size="22px"
-                              onClick={() => {
-                                const newCollection = [...collection]
-                                newCollection[i] =
-                                  newCollection[i] === 1 ? 0 : 1
-                                setcollection(newCollection)
-                              }}
-                            />
-                          ) : (
-                            <FcBookmark
-                              size="22px"
-                              onClick={() => {
-                                const newCollection = [...collection]
-                                newCollection[i] =
-                                  newCollection[i] === 1 ? 0 : 1
-                                setcollection(newCollection)
-                              }}
-                            />
-                          )}
-                        </div> */}
-                        <div className="add">加入收藏</div>
-                      </div>
-                    </div>
-                    <div class="line2 d-flex justify-content-between align-items-center border-bottom pb-3 pt-3">
-                      <div className="h6-tc d-flex">
-                        <div>
-                          {/* <GoLocation size="25px" /> */}
-                        </div>
-                        {/* {v.eventLocation} */}
-                      </div>
-                      <h6>尚有名額</h6>
-                    </div>
-                    <div class="line3 d-flex mt-3">
-                      {/* <div className="pr-3">{v.eventCategory} </div> */}
-                      <div>|</div>
-                      <div className="pl-3">一人成團</div>
-                    </div>
-                    <div className="more">
-                      <a href="#">MORE</a>
                     </div>
                   </div>
-                </div>
+                
             )
           })}
           <div></div>
